07dc47bd0eb0eb6eca521ae552b82fd977ea91e7
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-04-29 16:28:49 +0800

    Remove src/components/contracts/*

--- src/components/contracts/abis.ts
+++
@@ -1,9 +0,0 @@
-import abiArbitrumKovan4 from './abis/arbitrum-Inbox-Kovan-4.json'
-import abiOptimismKovan2 from './abis/optimism-OVM_L1ETHGateway-Kovan-2.json'
-
-const abis = {
-  abiArbitrumKovan4: abiArbitrumKovan4,
-  abiOptimismKovan2: abiOptimismKovan2
-}
-
-export default abis
