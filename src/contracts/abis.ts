1184ea18022802b09339ac037ff3ba90c97f05b6
Anton Cheng <antonassocareer@gmail.com>
2021-05-07 15:06:56 +0800

    fix: refactor constant and addresses export style

--- src/contracts/abis.ts
+++ src/contracts/abis.ts
@@ -1,12 +1,12 @@
-import abiArbitrumKovan4 from './abis/arbitrum-Inbox-Kovan-4.json'
-import abiOptimismKovan2 from './abis/optimism-OVM_L1ETHGateway-Kovan-2.json'
-import abiMaticMumbai from './abis/matic-RootChainManager-Mumbai.json'
+import arbitrumInbox from './abis/arbitrum-Inbox-Kovan-4.json'
+import optimismGateway from './abis/optimism-OVM_L1ETHGateway-Kovan-2.json'
+import maticBridge from './abis/matic-RootChainManager-Mumbai.json'
 import erc20 from './abis/erc20.json'
 
 const abis = {
-  abiArbitrumKovan4: abiArbitrumKovan4,
-  abiOptimismKovan2: abiOptimismKovan2,
-  abiMaticMumbai: abiMaticMumbai,
+  arbitrumInbox,
+  optimismGateway,
+  maticBridge,
   erc20
 }
 
