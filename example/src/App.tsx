891f74bf0e85ef4c9eff98077641558215762290
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2021-06-08 18:08:38 +0800

    Add basic WalletConnect Web3 & Client integration

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,9 +9,17 @@ import {
   Avalanche,
   MaticTestnet,
   Matic,
-  Config,
+  Config
 } from '@huskyfinance/eth-sorbet'
 import { WindowChain } from '../../src/types'
+
+// WalletConnect Client
+import WalletConnect from '@walletconnect/client'
+import QRCodeModal from '@walletconnect/qrcode-modal'
+
+// WalletConnect Web3
+import WalletConnectProvider from '@walletconnect/web3-provider'
+// import { providers } from 'ethers'
 
 const networks = [
   Binance,
@@ -35,25 +43,98 @@ const App = () => {
 
   useEffect(() => {
     const fetchData = async (provider: any) => {
-      const accounts = await provider.request({
-        method: 'eth_requestAccounts'
-      })
+      let accounts
+      if (provider.accounts) {
+        accounts = provider.accounts
+      } else {
+        accounts = await provider.request({
+          method: 'eth_requestAccounts'
+        })
+      }
       const account = accounts[0]
       setUserAddress(account)
     }
 
     if (!provider || !provider.request) {
-      const errorMessage =
-        "Please connect wallet first"
+      const errorMessage = 'Please connect wallet first'
       setUserAddress(errorMessage)
     } else {
       fetchData(provider)
+
+      // TODO: What is best way to identify if provider is WalletConnect
+      if (provider.bridge) {
+        // Subscribe to accounts change
+        provider.on('accountsChanged', (accounts: string[]) => {
+          console.log(accounts)
+        })
+        // Subscribe to chainId change
+        provider.on('chainChanged', (chainId: number) => {
+          console.log(chainId)
+        })
+        // Subscribe to session disconnection
+        provider.on('disconnect', (code: number, reason: string) => {
+          console.log(code, reason)
+        })
+      }
     }
   }, [provider])
 
   const connectMetamask = useCallback(() => {
     setProvider((window as WindowChain).ethereum)
   }, [])
+
+  const providerWalletConnect = new WalletConnectProvider({
+    infuraId: process.env.REACT_APP_INFURA_ID
+    // rpc: {
+    //   1: 'https://mainnet.mycustomnode.com',
+    //   3: 'https://ropsten.mycustomnode.com',
+    //   100: 'https://dai.poa.network'
+    //   // ...
+    // }
+  })
+
+  const connectWalletConnectWeb3 = useCallback(async () => {
+    await providerWalletConnect.enable()
+    setProvider(providerWalletConnect)
+
+    //  Wrap with Web3Provider from ethers.js
+    // const web3Provider = new providers.Web3Provider(providerWalletConnect)
+    // let balance = await web3Provider.getBalance('ethers.eth')
+    // console.log('balance: ', balance)
+    // console.log('network: ', await web3Provider.getNetwork())
+  }, [providerWalletConnect])
+
+  const connectWalletConnectClient = useCallback(() => {
+    // Create a connector
+    const connector = new WalletConnect({
+      bridge: 'https://bridge.walletconnect.org', // Required
+      qrcodeModal: QRCodeModal
+    })
+
+    // Check if connection is already established
+    if (!connector.connected) {
+      // create new session
+      connector.createSession()
+    }
+
+    // Subscribe to connection events
+    connector.on('connect', (error, payload) => {
+      if (error) {
+        throw error
+      }
+
+      // Get provided accounts and chainId
+      const { accounts, chainId } = payload.params[0]
+      console.log(chainId)
+      setUserAddress(accounts[0])
+    })
+  }, [])
+
+  const disconnect = async () => {
+    if (provider.disconnect) {
+      await provider.disconnect()
+    }
+  }
 
   const handleOpen = () => {
     setOpen(true)
@@ -85,8 +166,22 @@ const App = () => {
     <div>
       <h1>Test Site Title</h1>
       <button type='button' onClick={connectMetamask}>
-        Connect metamask
+        MetaMask - Connect
       </button>
+      <br />
+      <br />
+      <button type='button' onClick={connectWalletConnectWeb3}>
+        WalletConnect Web3 Provider - Connect
+      </button>
+      <br />
+      <button type='button' onClick={connectWalletConnectClient}>
+        WalletConnect Client - Connect
+      </button>
+      <br />
+      <button type='button' onClick={disconnect}>
+        WalletConnect - Disconnect
+      </button>
+      <br />
       <br />
       <button type='button' onClick={handleOpen}>
         Open Modal
